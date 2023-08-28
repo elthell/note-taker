e69d4b308ab29df591a776133922e02b190283b7
elthell <eleanor.thell@gmail.com>
2023-08-28 17:43:58 -0500

    server init, add gitignore + asset images

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,19 +8,19 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
-// `GET *` should return the `index.html` file.
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+app.use(express.static("public"));
 
 // `GET /notes` should return the `notes.html` file.
-app.get('/notes', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-// listening 
+// `GET *` should return the `index.html` file.
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// listening
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
